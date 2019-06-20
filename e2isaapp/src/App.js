703261075baa1d8c43fc2a12b27ed7aca4f22efe
nijoshi <niravhjoshi@outlook.com>
2019-06-20 11:35:54 +0530

    Added Map function and with button click event as class method for binding

--- e2isaapp/src/App.js
+++ e2isaapp/src/App.js
@@ -1,6 +1,19 @@
 import React,{Component} from 'react';
 import './App.css';
 
+//This way to create function do login inside function and return it values.
+/*const userService = {
+  getUserName: function (user){
+    return user.firstname + ' '+user.lastname;
+  },
+};
+
+//This where you can assign key to list
+const key = 'name';
+const user = {
+[key]: 'Robin',
+};
+*/
 const listvar = [
   {
   title: 'Learn Fucking React',
@@ -20,40 +33,52 @@ const listvar = [
   },
   ];
 
-class Developer
-{
-  constructor(firstname,lastname){
-  this.firstname = firstname;
-  this.lastname = lastname;
-  }
-  getName(){
-  return (this.firstname +' '+ this.lastname)
-  }
-}
 
-const newDev = new Developer("Nirav","Joshi")
-console.log(newDev.getName());
 class App extends Component
 {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      listvar
+    }
+
+    this.onDismiss= this.onDismiss.bind(this);
+  }
+  
+  onDismiss(id){
+    console.log(id)
+    const updatedList =this.state.listvar.filter(function isNotId(item){
+      return item.objectID !== id;
+      
+    });
+    console.log(updatedList)
+    this.setState({listvar: updatedList});
+  }
+
   render() {
-    const hellvar ="This is some New";
+    //const hellvar ="This is some New";
+    //This is where we will create method for detroying item from list upon click event on dismiss.
+
     return (
-           <div className="App">
+          <div className="App">
           <h2> Welcome to E2ISA App</h2>
           <header className="App-header">
-          {listvar.map(items => 
-          <div key={items.objectID}>
-          <code>{items.title}</code><p>
-          <code>{items.url}</code></p>
-          </div>
-
-          )}  
+          {this.state.listvar.map(item => 
+          
+          <code key={item.objectID}>
+           <p>{item.title}</p>
+           <p>{item.author}</p>
+           <button onClick={() => this.onDismiss(item.objectID)} type="button">
+            Dismiss
+          </button>
+          </code>  
+          
+          )}
           </header>
           </div>
-        
-      
-      
-  );
-}
+    );
+  }
 }
 export default App;
