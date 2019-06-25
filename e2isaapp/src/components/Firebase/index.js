e7e1aaf4e5e1248f83966b1849108dd9eb1eac00
nijoshi <niravhjoshi@outlook.com>
2019-06-25 08:48:35 +0530

    FireBaseConfig Added

--- e2isaapp/src/components/Firebase/index.js
+++ e2isaapp/src/components/Firebase/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import firebase, { app } from 'firebase';
 
-const Firebase = () => (
-    <div>
-        <h1>FireBase</h1>
-    </div>
-);
+const firebaseConfig = {
+    apiKey: "AIzaSyBFUTrGj8TtqqAjOsQlPcl82R7m1DEezEs",
+    authDomain: "myfiree2isa.firebaseapp.com",
+    databaseURL: "https://myfiree2isa.firebaseio.com",
+    projectId: "myfiree2isa",
+    storageBucket: "myfiree2isa.appspot.com",
+    messagingSenderId: "724251458480",
+    appId: "1:724251458480:web:109a977cbd59fe1b"
+  };
 
-export default Firebase;
+  class Firebase{
+      constructor(){
+      app.initializeApp(firebaseConfig);
+    }
+  }
+  
+  //firebase.initializeApp(firebaseConfig);
+
+  export default Firebase;
